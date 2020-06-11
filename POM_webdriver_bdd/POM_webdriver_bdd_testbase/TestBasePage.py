891f94aa071cb639e152c3891760bb8ad1562190
Noor.D@aricent.com <Noor.D@aricent.com>
2020-06-11 20:05:15 +0530

    base calss and event firing

---
+++ POM_webdriver_bdd/POM_webdriver_bdd_testbase/TestBasePage.py
@@ -0,0 +1,95 @@
+'''
+Created on Jun 8, 2020
+
+@author: NOORSHAVALI
+'''
+from configparser import ConfigParser
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+from selenium.webdriver.support.events import AbstractEventListener,EventFiringWebDriver
+from selenium.common.exceptions import TimeoutException
+from POM_webdriver_bdd.POM_webdriver_utils.WebElementEventListener import EventListeners
+
+import time
+
+'''
+## working configuration for new firefox browser driver
+
+cap = DesiredCapabilities().FIREFOX
+cap["marionette"] = False
+#chrome_options = Options()
+#chrome_options.add_argument('headless')
+#chrome_options.add_argument('--window-size = 1920*1080'
+
+driver = webdriver.Firefox(capabilities=cap, 
+                           executable_path="C:\\Users\\NOORSHAVALI\\Desktop\\NST\\Java\\geckodriver.exe")
+'''
+class BaseClass(object):
+    '''
+    classdocs
+    '''
+    args = []
+    parser =  driver = edriver = eventlistener = None
+    def __init__(self, *params):
+        args = params
+        '''
+        Constructor to configure and initialize webdriver which consists of 
+        eventfiringwebdriver and
+        abstracteventlistener
+        
+         
+        '''
+        
+        try:
+            BaseClass.parser = ConfigParser()
+            parser = BaseClass.parser
+            parser.read('../../POM_webdriver_bdd/pomconfig.ini')
+                    
+            if args[0] == 'page_delay_quick' or args[0] == 'page_delay_medium' or args[0] == 'page_delay_large':
+                dtime = (eval(parser['WEBTIMOUTS']['pageloadtimeout'].encode()))[args[0]]
+                dtime = int(dtime)
+               # return dtime
+            elif args[0] == 'implicit_delay':
+                pass
+                #return eval(parser['WEBTIMOUTS']['pageloadtimeout'].encode())
+            elif args[0] == 'URL':
+                URL = parser['BEHAV']['URL']
+                #return URL
+            else:
+                #return parser
+                pass
+        except IOError as e:
+            print("File open exception {}".format(e.args))
+            
+        except Exception as e:
+            print("File handling exception {}".format("e.args"))
+        
+    @staticmethod    
+    def initialization():  
+        try:
+            if BaseClass.parser["BEHAV"]["browser"] == "chrome":
+                driver = webdriver.Chrome(executable_path=r'C:\\Python27\\Scripts\\chromedriver.exe')
+                
+            elif BaseClass.parser["BEHAV"]["browser"] == "ff":
+                driver = webdriver.Firefox()
+            else:
+                pass
+            
+        except TimeoutException as e:
+            print("File open exception {}".format(e.args))
+            
+        except Exception as e:
+            print("File handling exception {}".format(e.args))
+            
+        edriver = EventFiringWebDriver(driver,EventListeners())  
+        driver = edriver
+        driver.get(BaseClass.parser['BEHAV']['URL'])
+        edriver.maximize_window()
+        time.sleep(10)
+        
+if __name__ == "__main__":
+    baseobj = BaseClass('page_delay_quick')
+    BaseClass.initialization()
+        
+         
---
+++ POM_webdriver_bdd/POM_webdriver_utils/WebElementEventListener.py
@@ -0,0 +1,103 @@
+'''
+Created on Jun 9, 2020
+
+@author: NOORSHAVALI
+'''
+
+from selenium import webdriver
+import time
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+#from pynput.mouse import Listener
+
+#b = webdriver.Chrome(executable_path=r'C:\\Python27\\Scripts\\chromedriver.exe')
+#b.maximize_window()
+
+class EventListeners(AbstractEventListener):
+    def before_navigate_to(self, url, driver):
+        print("-before_navigate_to %s" % url)
+
+    def after_navigate_to(self, url, driver):
+        print("-after_navigate_to %s" % url)
+
+    def before_click(self, element, driver):
+        print("before_click %s" % element)
+
+    def after_click(self, element, driver):
+        print("after_click %s" %element)
+
+    def after_navigate_forward(self, driver):
+        print("after_navigate_forward");
+
+    def before_navigate_forward(self, driver):
+        print("before_navigate_forward")
+
+    def after_navigate_back(self, driver):
+        print("after_navigate_back")
+
+    def before_navigate_back(self, driver):
+        print("before_navigate_back")
+
+    def before_change_value_of(self, element, driver):
+        print("before_change_value_of")
+    def on_exception(self, exception, driver):
+        print("on_exception")
+        
+        
+
+class MyListener(AbstractEventListener):
+    def before_navigate_to(self, url, driver):
+        print("Before navigating to ", url)
+
+    def after_navigate_to(self, url, driver):
+        print("After navigating to ", url)
+
+    def before_navigate_back(self, driver):
+        print("before navigating back ", driver.current_url)
+
+    def after_navigate_back(self, driver):
+        print("After navigating back ", driver.current_url)
+
+    def before_navigate_forward(self, driver):
+        print("before navigating forward ", driver.current_url)
+
+    def after_navigate_forward(self, driver):
+        print("After navigating forward ", driver.current_url)
+
+    def before_find(self, by, value, driver):
+        print("before find")
+
+    def after_find(self, by, value, driver):
+        print("after_find")
+
+    def before_click(self, element, driver):
+        print("before_click")
+
+    def after_click(self, element, driver):
+        print("after_click")
+
+    def before_change_value_of(self, element, driver):
+        print("before_change_value_of")
+
+    def after_change_value_of(self, element, driver):
+        print("after_change_value_of")
+
+    def before_execute_script(self, script, driver):
+        print("before_execute_script")
+
+    def after_execute_script(self, script, driver):
+        print("after_execute_script")
+
+    def before_close(self, driver):
+        print("tttt")
+
+    def after_close(self, driver):
+        print("before_close")
+
+    def before_quit(self, driver):
+        print("before_quit")
+
+    def after_quit(self, driver):
+        print("after_quit")
+
+    def on_exception(self, exception, driver):
+        print("on_exception")
